ee7349995852c612fffebc3fa28baf7381925407
秦正楠 <qznqzr_371521@163.com>
2019-08-12 22:53:36 +0800

    test

---
+++ src/main/java/com/how2java/springboottest/SpringboottestApplication.java
@@ -0,0 +1,11 @@
+package com.how2java.springboottest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringboottestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringboottestApplication.class, args);
+    }
+}
